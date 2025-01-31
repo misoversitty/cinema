d523d131a0a63394378905ef351e3dfaee030af0
misoversit <smirennikov.i@mail.ru>
2025-02-01 02:48:53 +0300

    Add new schemas to deserialize objects

--- src/modelSchemas.py
+++ src/modelSchemas.py
@@ -11,10 +11,22 @@ customers_schema = CustomerSchema(many=True)
 class FilmSchema(Schema):
     film_id = fields.Int()
     name = fields.String()
-    #sessions = fields.List(fields.Nested("FilmSessionSchema"))
+    sessions = fields.List(fields.Nested("FilmSessionSchema", exclude=["film"]))
 film_schema = FilmSchema()
 films_schema = FilmSchema(many=True)
 
 
 class OrderSchema(Schema):
     order_id = fields.Int()
+    sessions = fields.List(fields.Nested("FilmSessionSchema", exclude=["orders"]))
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+
+
+class FilmSessionSchema(Schema):
+    session_id = fields.Int()
+    film = fields.Nested("FilmSchema", exclude=["sessions"])
+    orders = fields.List(fields.Nested("OrderSchema", exclude=["sessions"]))
+    include_relationships = True
+filmSession_schema = FilmSessionSchema()
+filmSessions_schema = FilmSessionSchema(many=True)
