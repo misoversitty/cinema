73c14bb53ff57e23bf91926a79834881ced2300a
misoversit <smirennikov.i@mail.ru>
2025-02-05 19:54:49 +0300

    Add customers view

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import requests
 from connexion import FlaskApp
 from connexion.resolver import MethodResolver
+from flask import render_template
 
 from config import *
 
@@ -10,6 +12,13 @@ app.add_api("swagger.yml", resolver=MethodResolver('controllers'))
 
 def main():
     app.run(host="0.0.0.0", port=8000)
+
+
+@app.route("/customers")
+def show_customers():
+    request = requests.get("http://localhost:8000/api/customers")
+    customers = request.json()
+    return render_template("customers.html", customers=customers)
 
 
 if __name__ == "__main__":
