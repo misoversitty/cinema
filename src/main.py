dbedf5a222ec05bb3397b793d10dc872a0dca9bc
misoversit <smirennikov.i@mail.ru>
2025-02-05 19:53:11 +0300

    Refactor. Change names of variables

--- src/main.py
+++ src/main.py
@@ -4,13 +4,12 @@ from connexion.resolver import MethodResolver
 from config import *
 
 
-connexion_app = FlaskApp(__name__, specification_dir=SPECIFICATION_PATH)
-connexion_app.add_api("swagger.yml", resolver=MethodResolver('controllers'))
-app = connexion_app.app
+app = FlaskApp(__name__, specification_dir=SPECIFICATION_PATH)
+app.add_api("swagger.yml", resolver=MethodResolver('controllers'))
 
 
 def main():
-    connexion_app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
