cd1cc88f79d712df3b8a9f728c0685605e7678c6
misoversit <smirennikov.i@mail.ru>
2025-02-08 02:20:09 +0300

    Add response codes

--- src/controllers.py
+++ src/controllers.py
@@ -12,14 +12,14 @@ class FilmsController:
         with Session(engine) as session:
             stmt = select(Film)
             films = session.execute(stmt).scalars()
-            return films_schema.dump(films)
+            return films_schema.dump(films), 200
 
     @staticmethod
     def read_one(film_id: int):
         with Session(engine) as session:
             stmt = select(Film).where(Film.film_id == film_id)
             film = session.execute(stmt).scalar()
-            return film_schema.dump(film)
+            return film_schema.dump(film), 200
 
 
 class FilmSessionsController:
@@ -28,14 +28,14 @@ class FilmSessionsController:
         with Session(engine) as session:
             stmt = select(FilmSession)
             filmSessions = session.execute(stmt).scalars()
-            return filmSessions_schema.dump(filmSessions)
+            return filmSessions_schema.dump(filmSessions), 200
 
     @staticmethod
     def read_one(filmsession_id: int):
         with Session(engine) as session:
             stmt = select(FilmSession).where(FilmSession.session_id == filmsession_id)
             filmsession = session.execute(stmt).scalar()
-            return filmSession_schema.dump(filmsession)
+            return filmSession_schema.dump(filmsession), 200
 
 
 class OrdersController:
@@ -44,14 +44,14 @@ class OrdersController:
         with Session(engine) as session:
             stmt = select(Order)
             orders = session.execute(stmt).scalars()
-            return orders_schema.dump(orders)
+            return orders_schema.dump(orders), 200
 
     @staticmethod
     def read_one(order_id: int):
         with Session(engine) as session:
             stmt = select(Order).where(Order.order_id == order_id)
             order = session.execute(stmt).scalar()
-            return order_schema.dump(order)
+            return order_schema.dump(order), 200
 
     @staticmethod
     def create(*args, **kwargs):
@@ -78,14 +78,14 @@ class CustomersController:
         with Session(engine) as session:
             stmt = select(Customer)
             customers = session.execute(stmt).scalars()
-            return customers_schema.dump(customers)
+            return customers_schema.dump(customers), 200
 
     @staticmethod
     def read_one(customer_id: int):
         with Session(engine) as session:
             stmt = select(Customer).where(Customer.customer_id == customer_id)
             customer = session.execute(stmt).scalar()
-            return customer_schema.dump(customer)
+            return customer_schema.dump(customer), 200
 
     @staticmethod
     def create(*args, **kwargs):
@@ -109,11 +109,11 @@ class TicketsController:
         with Session(engine) as session:
             stmt = select(Ticket)
             tickets = session.execute(stmt).scalars()
-            return tickets_schema.dump(tickets)
+            return tickets_schema.dump(tickets), 200
 
     @staticmethod
     def read_one(ticket_id: int):
         with Session(engine) as session:
             stmt = select(Ticket).where(Ticket.ticket_id == ticket_id)
             ticket = session.execute(stmt).scalar()
-            return ticket_schema.dump(ticket)
+            return ticket_schema.dump(ticket), 200
