4281477a3b2238c9c6e63cf8ddc7dc7b301b143b
misoversit <smirennikov.i@mail.ru>
2025-02-09 17:39:03 +0300

    Refactor. View routes moved to views file

--- src/main.py
+++ src/main.py
@@ -1,73 +1,17 @@
-import requests
 from connexion import FlaskApp
 from connexion.resolver import MethodResolver
-from flask import render_template, request
 
 from config import *
+from views import registryViews
 
 
 app = FlaskApp(__name__, specification_dir=SPECIFICATION_PATH)
 app.add_api("swagger.yml", resolver=MethodResolver('controllers'))
+registryViews(app)
 
 
 def main():
     app.run(host="0.0.0.0", port=8000)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/customers")
-def show_customers():
-    customers = requests.get("http://localhost:8000/api/customers").json()
-    return render_template("customers.html", customers=customers)
-
-
-@app.route("/customers/<int:id>")
-def show_customer(id):
-    if request.method == "GET":
-        customer = requests.get(f"http://localhost:8000/api/customers/{id}").json()
-        return render_template("customer.html", customer=customer)
-    elif request.method == "POST":
-        pass
-
-
-@app.route("/orders")
-def show_orders():
-    orders = requests.get("http://localhost:8000/api/orders").json()
-    return render_template("orders.html", orders=orders)
-
-
-@app.route("/orders/<int:id>")
-def show_order(id):
-    if request.method == "GET":
-        order = requests.get(f"http://localhost:8000/api/orders/{id}").json()
-        return render_template("order.html", order=order)
-    elif request.method == "POST":
-        pass
-
-
-@app.route("/filmsessions")
-def show_filmsessions():
-    sessions = requests.get("http://localhost:8000/api/filmsessions").json()
-    return render_template("filmsessions.html", sessions=sessions)
-
-
-@app.route("/filmsessions/<int:id>")
-def show_filmsession(id):
-    if request.method == "GET":
-        session = requests.get(f"http://localhost:8000/api/filmsessions/{id}").json()
-        return render_template("filmsession.html", session=session)
-    elif request.method == "POST":
-        pass
-
-
-@app.route("/films")
-def show_films():
-    films = requests.get("http://localhost:8000/api/films").json()
-    return render_template("films.html", films=films)
 
 
 if __name__ == "__main__":
