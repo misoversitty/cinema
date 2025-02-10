0660b44c311b4ff9b9b1512428d613f50476cae3
misoversit <smirennikov.i@mail.ru>
2025-02-11 00:23:28 +0300

    Add authentication commands

--- src/controllers.py
+++ src/controllers.py
@@ -4,6 +4,18 @@ from sqlalchemy import select
 from models import *
 from modelSchemas import *
 from config import engine
+from utils import hash_password
+
+
+class AuthenticationController:
+    @staticmethod
+    def authenticate(username: str, password: str):
+        with Session(engine) as session:
+            user = session.scalar(select(User)
+                                  .where(User.username == username,
+                                         User.password_hash == hash_password(password)))
+            if user is not None:
+                return {"sub": user.user_id}
 
 
 class FilmsController:
