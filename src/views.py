ecb93ab9f25d76ad1ced7a28ed7d50d1ecfc9b9c
misoversit <smirennikov.i@mail.ru>
2025-02-16 19:14:56 +0300

    Add customer creating

--- src/views.py
+++ src/views.py
@@ -1,7 +1,9 @@
 import requests
 from connexion import FlaskApp
-from flask import render_template
 from flask import request
+from flask import render_template
+from flask import redirect
+from flask import url_for
 
 
 def registryViews(app: FlaskApp):
@@ -21,6 +23,24 @@ def registryViews(app: FlaskApp):
             return render_template("customer.html", customer=customer)
         elif request.method == "POST":
             pass
+
+    @app.route("/customers/add_new", methods=["POST", "GET"])
+    def add_customer():
+        if request.method == "POST":
+            last_name = request.form.get("last_name")
+            first_name = request.form.get("first_name")
+            middle_name = request.form.get("middle_name")
+            data = {"last_name": last_name,
+                    "first_name": first_name,
+                    "middle_name": middle_name}
+            res = requests.post(f"http://localhost:8000/api/customers", json=data)
+            if res.status_code == 201:
+                return redirect(url_for("customer_created"))
+        return render_template("customer.html", customer=None)
+
+    @app.route("/customers/add_new/success")
+    def customer_created():
+        return render_template("customer_created.html")
 
     @app.route("/orders")
     def show_orders():
