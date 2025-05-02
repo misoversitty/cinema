3a9a631d71b39d223517daf86510d5e7dc00d219
misoversit <smirennikov.i@mail.ru>
2025-05-02 22:26:53 +0300

    Use host/port from config file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from core import app
 from core_views.views import sub_app
+from config import host_address
+from config import host_port
 
 
 app.app.register_blueprint(sub_app)
 
 
 def main():
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host=host_address, port=host_port)
 
 
 if __name__ == "__main__":
